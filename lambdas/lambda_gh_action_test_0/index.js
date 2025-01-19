0e600111335eba7b807150c293f85ee7d5939fd9
kailasrj-rg <kailas@revyrieglobal.com>
2025-01-19 16:16:24 +0530

    first commit

--- lambdas/lambda_gh_action_test_0/index.js
+++ lambdas/lambda_gh_action_test_0/index.js
@@ -1,7 +1,7 @@
 import * as _ from 'lodash';
 
 export const handler = async (event) => {
-    // Lamda TEST 0 !!!!!!!!!!
+    // Lamda TEST 0 !!!!!!!!!!!
     const max = 100;
     const val = _.random(max);
     console.log('will this work?')
