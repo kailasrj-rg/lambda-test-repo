0bd20b0a7b68875559c2f9f6cbb9d81fed093b54
kailasrj-rg <kailas@revyrieglobal.com>
2025-01-19 20:25:04 +0530

    first commit

--- lambdas/lambda_gh_action_test_0/index.js
+++ lambdas/lambda_gh_action_test_0/index.js
@@ -1,14 +1,12 @@
-import * as _ from 'lodash';
+import _ from 'lodash';
 
 export const handler = async (event) => {
-    // Lamda TEST 0 !!!!!!!!!!!!
-    const max = 100;
-    const val = _.random(max);
-    console.log('will this work?')
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(`Hello from Test Lambda! max: ${max} is ${val}`),
-    };
-    return response;
+  // Test 0
+  const max = 100;
+  const val = _.random(max);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(`Hello from Test Lambda! max: ${max} is ${val}`),
   };
-  
+  return response;
+};
--- lambdas/lambda_gh_action_test_1/index.js
+++ lambdas/lambda_gh_action_test_1/index.js
@@ -1,13 +1,12 @@
-import * as _ from 'lodash';
+import _ from 'lodash';
 
 export const handler = async (event) => {
-    // Lamda TEST 1 !!!!!!!!!!!
-    const max = 100;
-    const val = _.random(max);
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(`Hello from Test Lambda! max: ${max} is ${val}`),
-    };
-    return response;
+  // Test 1
+  const max = 100;
+  const val = _.random(max);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(`Hello from Test Lambda! max: ${max} is ${val}`),
   };
-  
+  return response;
+};
